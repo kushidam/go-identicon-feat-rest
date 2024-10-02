9235dfb655476f51f40242e1de93f37b1f364b9e
kushidam <131370475+kushidam@users.noreply.github.com>
2024-10-02 13:38:13 +0000

    [Add] endpoint

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -13,6 +13,9 @@ func (s *Server) RegisterRoutes() http.Handler {
 	e.Use(middleware.Recover())
 
 	e.GET("/", s.HelloWorldHandler)
+	
+	// IdentIconを作成するAPIエンドポイント（CreateIdentIconHandler）を追加
+	e.POST("/identicon", s.CreateIdentIconHandler)
 
 	e.GET("/health", s.healthHandler)
 
@@ -29,4 +32,28 @@ func (s *Server) HelloWorldHandler(c echo.Context) error {
 
 func (s *Server) healthHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, s.db.Health())
+}
+// IdentIconを作成するエンドポイント
+// これはユーザが会員登録した際に、ユーザIDに応じて一意の画像を生成する。
+// request：ユーザID
+// response：成功/失敗ステータス
+// 【処理内容】
+// 1. ユーザIDを受け取る
+// 2. ユーザIDを元にIdentIconを生成する
+// 3. 生成したIdentIconをDBに格納する
+// 4. 成功/失敗ステータスを返す
+// 5. エラーが発生した場合はエラーメッセージを返す
+// 6. ユーザIDが重複している場合はエラーメッセージを返す
+// 7. ユーザIDが重複していない場合は成功ステータスを返す
+func (s *Server) CreateIdentIconHandler(c echo.Context) error {
+	// ユーザIDを受け取る
+	
+	// ユーザIDを元にIdentIconを生成する
+	
+	
+	// 生成したIdentIconをDBに格納する
+	
+	
+	// 成功/失敗ステータスを返す
+	return c.JSON(http.StatusOK, "success")
 }
